10ba7ac3ef0b35937666ea5e6403d9d0fd12ac5b
kang-heewon <itphantom@icloud.com>
2019-06-08 19:04:10 +0900

    웰컴 메시지 추가

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,6 +3,10 @@ import iotRouter from './iot';
 
 const router = express.Router();
 
+router.get('/', (_, res) => {
+  res.send('BISS Backend Server');
+});
+
 router.use('iot', iotRouter);
 
 export default router;
